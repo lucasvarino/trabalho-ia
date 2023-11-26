b68ff72d498dc2ece6853f3f4cd6e45ab5354a31
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2023-11-26 16:56:22 -0300

    add versao inicial da busca em largura

--- main.py
+++ main.py
@@ -26,14 +26,49 @@ def backtracking(state: State, i, history=[]):
     else:
         return backtracking(state, i+1, history)
 
+def largura():
+    abertos = []
+    state = State()
+    fracasso = False
+    sucesso = False
+    abertos.append(state)
+    print(f'{abertos=}\n')
+    fechados = []
 
+    while(not(fracasso or sucesso)):
+        if len(abertos) == 0:
+            fracasso = True
+            print(f'{fracasso=}\n')
+        else:
+            n = abertos[0]
+            print(f'{n=}\n')
+            abertos.pop(0)
+            if n.is_complete():
+                sucesso = True
+                print(f'{sucesso=}\n')
+            else:
+                for rule in rules:
+                    #print('For das regras')
+                    new_state = state.apply_rule(rule)     
+                    if new_state is not None and not state.is_in_history(rule,fechados):
+                        print('Entrou no IF new_state')
+                        abertos.append(new_state)
+                        print(f'{abertos=}\n')
+                fechados.append(n)
+        print(new_state)
+    
+    if sucesso:
+        return n
+    else:
+        return None
 
 def main():
     state = State()
     history = []
     history.append(state)
     # print(state)
-    backtracking(state, 0, history)
+    #backtracking(state, 0, history)
+    state = largura()
 
     # state = state.apply_rule(rules[0])
     # state = state.apply_rule(rules[1])
@@ -41,7 +76,7 @@ def main():
     # state = state.apply_rule(rules[1])
     # state = state.apply_rule(rules[3])
     # print(state.is_complete())
-    print(state);
+    print(state)
 
 
 if __name__ == "__main__":
