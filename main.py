bda6689cce59e1b72181acd623f2510aa0b441fd
Lucas Varino <lucasvarino@gmail.com>
2023-11-26 15:59:14 -0300

    Regras arrumadas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from rule import Rule
 from state import State
 from member import Membro
 
-rules = [Rule(Membro.FILHO, Membro.FILHO), Rule(Membro.FILHO), Rule(Membro.FILHO, Membro.PAI), Rule(Membro.FILHO, Membro.MAE)]
+rules = [Rule(Membro.FILHO, Membro.FILHO), Rule(Membro.FILHO), Rule(Membro.PAI), Rule(Membro.MAE)]
 
 def backtracking(state: State, i, history=[]):
     if state.is_complete():
@@ -33,13 +33,17 @@ def main():
     history = []
     history.append(state)
     # print(state)
-    backtracking(state, 0, history)
+    # backtracking(state, 0, history)
 
     # state = state.apply_rule(rules[0])
     # state = state.apply_rule(rules[1])
     # state = state.apply_rule(rules[2])
     # state = state.apply_rule(rules[1])
+    # state = state.apply_rule(rules[0])
+    # state = state.apply_rule(rules[1])
     # state = state.apply_rule(rules[3])
+    # state = state.apply_rule(rules[1])
+    # state = state.apply_rule(rules[0])
     # print(state.is_complete())
     print(state);
 
