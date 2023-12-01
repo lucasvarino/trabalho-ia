ad8213469929f83de4fedd8a784fc7ded544bd46
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2023-12-01 16:19:28 -0300

    primeira tentativa de uma Aestrela, sem sucesso

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from rule import Rule
 from state import State
 from member import Membro
+import heapq
 
 rules = [Rule(Membro.FILHO, Membro.FILHO), Rule(Membro.FILHO), Rule(Membro.PAI), Rule(Membro.MAE)]
 
@@ -31,6 +32,28 @@ def heuristic(state: State) -> int:
     # Exemplo de heurística: número de membros no lado direito do rio
     return len(state.right)
 
+def astar_search(initial_state: State):
+    # Utilizando uma fila de prioridade (heapq) para armazenar os estados a serem explorados
+    open_set = [(heuristic(initial_state), initial_state)]
+    # Conjunto de estados já explorados
+    closed_set = set()
+
+    while open_set:
+        _, current_state = heapq.heappop(open_set)
+
+        if current_state.is_complete():
+            return current_state
+
+        closed_set.add(hash(current_state))
+
+        for i, rule in enumerate(rules):
+            if current_state.is_valid(rule):
+                new_state = current_state.apply_rule(rule)
+                if hash(new_state) not in closed_set:
+                    heapq.heappush(open_set, (heuristic(new_state), new_state))
+
+    return None
+
 
 def main():
     state = State()
@@ -49,7 +72,15 @@ def main():
     # state = state.apply_rule(rules[1])
     # state = state.apply_rule(rules[0])
     # print(state.is_complete())
-    print(state);
+    
+    # Utilizando A* para encontrar a solução
+    solution = astar_search(state)
+
+    if solution:
+        print("Solução encontrada:")
+        print(solution)
+    else:
+        print("Não foi possível encontrar uma solução.")
 
 
 if __name__ == "__main__":
